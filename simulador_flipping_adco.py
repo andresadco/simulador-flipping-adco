b4014249ef21b97994b320bf074b7658d19b7b2f
andresadco <andres@adco.es>
2025-05-14 12:05:54 +0200

    Update simulador_flipping_adco.py

--- simulador_flipping_adco.py
+++ simulador_flipping_adco.py
@@ -143,7 +143,7 @@ resumen_data = {
         f"{ganancia_neta:,.0f}",
         f"{roi:.2f}",
         f"{tir:.2f}"
-        f"{precio_venta:/superficie}"
+        f"{precio_venta:,/superficie}"
     ]
 }
 df_resumen = pd.DataFrame(resumen_data)
